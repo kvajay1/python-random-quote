c68d70d9b0a20517602e43c3e08646b6c9934585
kvajay1 <70701350+kvajay1@users.noreply.github.com>
2020-09-07 23:16:20 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,11 @@
 
  def main():
-  f = open("quotes.txt")
-  quotes = f.readlines()
-   f.close()
+ print(hi)
+ # f = open("quotes.txt")
+  #quotes = f.readlines()
+   #f.close()
 
-  print(quotes[0])
+  #print(quotes[0])
  if __name__== "__main__":
   main()
 

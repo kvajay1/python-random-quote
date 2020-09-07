5474007070ed22f3d1684b6579a06a415fde2c7c
kvajay1 <70701350+kvajay1@users.noreply.github.com>
2020-09-07 23:29:01 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
 
  def main():
- 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-   f.close()
+ git add get-quote.py
+ git commit -m "Random quote bot is working"
+git push
+ # f = open("quotes.txt")
+  #quotes = f.readlines()
+   #f.close()
 
-  print(quotes[0])
+  #print(quotes[0])
  if __name__== "__main__":
   main()
 
